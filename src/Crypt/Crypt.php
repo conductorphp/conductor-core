da3d57858ce88fcb864a4112daa7c9308058072e
Kirk <kmadera@robofirm.com>
2018-03-21 13:40:59 -0400

    Fixed logic when crypt_key not set

--- src/Crypt/Crypt.php
+++ src/Crypt/Crypt.php
@@ -56,17 +56,13 @@ class Crypt
         return function () use ($config, $cryptKey) {
             $crypt = new self();
             $decryptConfig = function ($data) use (&$decryptConfig, $crypt, $cryptKey) {
-                if (is_null($cryptKey)) {
-                    return $data;
-                }
-
                 if (is_array($data)) {
                     foreach ($data as $key => &$value) {
                         $value = $decryptConfig($value);
                     }
                     unset($value);
                 } else {
-                    if (preg_match('/^ENC\[defuse\/php-encryption,.*\]/', $data)) {
+                    if (!is_null($cryptKey) && preg_match('/^ENC\[defuse\/php-encryption,.*\]/', $data)) {
                         $data = preg_replace('/^ENC\[defuse\/php-encryption,(.*)\]/', '$1', $data);
                         $data = $crypt->decrypt($data, $cryptKey);
                     }
