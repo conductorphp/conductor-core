d5cf72ca07a1fcd0050cc952436c3168772271f6
Kirk Madera <kirk.madera@rmgmedia.com>
2025-10-20 22:47:01 -0400

    Fixed default file visibility

--- config/filesystem.php
+++ config/filesystem.php
@@ -5,7 +5,14 @@ return [
         'local' => [
             'class'     => \League\Flysystem\Local\LocalFilesystemAdapter::class,
             'arguments' => [
-                'location'          => '/',
+                'location'     => '/',
+                'visibility'   => new \League\Flysystem\UnixVisibility\PortableVisibilityConverter(
+                    filePublic: 0644,
+                    filePrivate: 0600,
+                    directoryPublic: 0755,
+                    directoryPrivate: 0700,
+                    defaultForDirectories: \League\Flysystem\Visibility::PUBLIC
+                ),
                 'linkHandling' => \League\Flysystem\Local\LocalFilesystemAdapter::SKIP_LINKS,
             ],
         ],
